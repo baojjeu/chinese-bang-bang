d6707b496e2c32d4d6da99a2cd635cfcf82f0282
Baozi Wu <baojjeu@gmail.com>
2014-12-28 14:48:42 +0800

    Revised model spec

--- spec/models/example_spec.rb
+++ spec/models/example_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe Example, type: :model do
   it "generate a pinyin before creating" do
     topic = FactoryGirl.create(:topic)
-    example = FactoryGirl.create(:example, topic: topic)
+    example = FactoryGirl.create(:example, topic: topic, sentence: '弟弟刚刚把拉面吃掉了。')
 
-    expect(example.hanyu.try(:pinyin)).to eql PinYin.sentence(example.sentence, :unicode)
+    expect(example.hanyu.try(:pinyin)).to eql 'dì di gāng gāng bǎ lā miàn chī diào le.'
   end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Topic, :type => :model do
   describe "Hanyu Pinyin" do
     it "generate a pinyin before creating" do
-      topic = FactoryGirl.create(:topic)
+      topic = FactoryGirl.create(:topic, name: '刚刚')
 
-      expect(topic.hanyu.try(:pinyin)).to eql PinYin.sentence(topic.name, :unicode)
+      expect(topic.hanyu.try(:pinyin)).to eql 'gāng gāng'
     end
   end
 end
