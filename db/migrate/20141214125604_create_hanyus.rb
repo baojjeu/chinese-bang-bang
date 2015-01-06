72aac5341ecaaa6ba48f653e2fc262da4f9acd75
Baozi Wu <baojjeu@gmail.com>
2015-01-07 00:15:51 +0800

    Update migration

--- db/migrate/20141214125604_create_hanyus.rb
+++ db/migrate/20141214125604_create_hanyus.rb
@@ -1,8 +1,5 @@
 class CreateHanyus < ActiveRecord::Migration
   def change
-    drop_table :hanyus
-    drop_table :hanyupinyins
-
     create_table :hanyus do |t|
       t.string :pinyin
       t.references :pinyinable, polymorphic: true
