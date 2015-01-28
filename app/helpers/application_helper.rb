8731ec020e37b5acec13a27a44b8bad8b51760da
Baozi Wu <baojjeu@gmail.com>
2015-01-28 11:06:08 +0800

    Rename method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     content.gsub(/`([^`]*)`/, '<code>\1</code>').html_safe
   end
 
-  def split_topic(name)
+  def generate_pinyin_ignore_sym_for(name)
     name.scan(/[^。，\x00-\x7F]+/).join.split('')
   end
 end
